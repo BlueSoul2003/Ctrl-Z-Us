03a2336347eae2ef0f6a5728ed3b2fbdc09f1444
AngieWong630 <148403271+AngieWong630@users.noreply.github.com>
2025-09-04 22:38:35 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Ctrl-Z-Us/', 
   css: {
     postcss: './postcss.config.js',
   },
